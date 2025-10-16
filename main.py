6a9843a3ef5bd05b9bc6446d8463dafc7c168e08
zhen6.gao <zhen6.gao@getech.cn>
2025-10-16 10:16:20 +0800

    ccrcc 本地用 CPU 跑需要改动的东西

--- main.py
+++ main.py
@@ -154,8 +154,8 @@ print('\nLoad Dataset')
 
 if args.task == 'task_1_tumor_vs_normal':
     args.n_classes=2
-    dataset = Generic_MIL_Dataset(csv_path = 'dataset_csv/tumor_vs_normal_dummy_clean.csv',
-                            data_dir= os.path.join(args.data_root_dir, 'tumor_vs_normal_resnet_features'),
+    dataset = Generic_MIL_Dataset(csv_path = 'dataset_ccrcc/tumor_vs_normal_dummy_clean.csv',
+                            data_dir= os.path.join(args.data_root_dir, ''),
                             shuffle = False, 
                             seed = args.seed, 
                             print_info = True,
