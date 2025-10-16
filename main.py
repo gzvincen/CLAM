4c8d98af4962c9ad1d4144c197e336bdab4c1aed
zhen6.gao <zhen6.gao@getech.cn>
2025-10-16 14:24:38 +0800

    ccrcc uni2 调整

--- main.py
+++ main.py
@@ -155,7 +155,7 @@ print('\nLoad Dataset')
 if args.task == 'task_1_tumor_vs_normal':
     args.n_classes=2
     dataset = Generic_MIL_Dataset(csv_path = 'dataset_ccrcc_uni2/tumor_vs_normal_dummy_clean.csv',
-                            data_dir= os.path.join(args.data_root_dir, 'tumor_vs_normal_resnet_features'),
+                            data_dir= os.path.join(args.data_root_dir, ''),
                             shuffle = False, 
                             seed = args.seed, 
                             print_info = True,
